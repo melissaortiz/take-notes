6e6187e3a6861a9ea8d8e5d4eb620269907425e8
melissaortiz <melissaortiz.dev@gmail.com>
2019-06-02 12:07:35 -0700

    changed ternary conditions

--- notes-edit.js
+++ notes-edit.js
@@ -4,9 +4,7 @@ const removeButton = document.querySelector('#remove-note')
 const dateElement = document.querySelector('#updated-at')
 const noteId = location.hash.substring(1)
 let notes = getSavedNotes()
-let note = notes.find(function (note) {
-    return note.id === noteId
-})
+let note = notes.find((note) => note.id === noteId)
 
 if (note === undefined){
     location.assign('/notes-app/index.html')
@@ -26,7 +24,7 @@ titleElement.addEventListener('input', function (e){
 })
 
 // save body on the fly
-bodyElement.addEventListener('input', function (e) {
+bodyElement.addEventListener('input', (e) => {
     note.body = e.target.value
     note.updatedAt = moment().valueOf()
     dateElement.textContent = generateLastEdited(note.updatedAt)
@@ -34,13 +32,13 @@ bodyElement.addEventListener('input', function (e) {
 })
 
 // remove button
-removeButton.addEventListener('click', function (e) {
+removeButton.addEventListener('click', (e) => {
     removeNote(note.id)
     saveNotes(notes)
     location.assign('/notes-app/index.html')
 })
 
-window.addEventListener('storage', function (e) {
+window.addEventListener('storage', (e) => {
     if (e.key === 'notes'){
        notes = JSON.parse(e.newValue)
        note = notes.find(function (note) {
